a5740e56a4faaad4de169ea4db2951e66e45b3ab
tamarindsoup <san.jose3993@gmail.com>
2018-08-22 18:04:59 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+PROJECT_NAME='pathcmd'
+VERSION='1.0'
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+     long_description = f.read()
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    description='Cmd module with path completion',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kenta Suzuki',
+    author_email='san.jose3993@gmail.com',
+    packages=find_packages(),
+    )
